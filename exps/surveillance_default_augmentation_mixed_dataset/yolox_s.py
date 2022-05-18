cae9f4a92c8b2693f839f8808d1184ea495c80a5
Jai Doshi <jdoshi8@gmail.com>
2022-05-18 07:25:45 +0000

    added custom augmentation exp

--- exps/surveillance_default_augmentation_mixed_dataset/yolox_s.py
+++ exps/surveillance_default_augmentation_mixed_dataset/yolox_s.py
@@ -47,24 +47,23 @@ class Exp(MyExp):
         self.test_ann = "Combined-DET-test.json"
 
         # --------------- transform config ----------------- #
-        # prob of applying mosaic aug
-        self.mosaic_prob = 0
+        self.mosaic_prob = 1.0
         # prob of applying mixup aug
-        self.mixup_prob =0
+        self.mixup_prob = 1.0
         # prob of applying hsv aug
-        self.hsv_prob = 0
+        self.hsv_prob = 1.0
         # prob of applying flip aug
-        self.flip_prob = 0
+        self.flip_prob = 0.5
         # rotation angle range, for example, if set to 2, the true range is (-2, 2)
-        self.degrees = 0
+        self.degrees = 10.0
         # translate range, for example, if set to 0.1, the true range is (-0.1, 0.1)
-        self.translate = 0
+        self.translate = 0.1
         self.mosaic_scale = (0.1, 2)
         # apply mixup aug or not
-        self.enable_mixup = False
+        self.enable_mixup = True
         self.mixup_scale = (0.5, 1.5)
         # shear angle range, for example, if set to 2, the true range is (-2, 2)
-        self.shear = 0
+        self.shear = 2.0
 
         # --------------  training config --------------------- #
         # epoch number used for warmup
